c384c86e16293d577201e8bfde41cb261ecdbb10
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-04 13:35:25 +0100

    Update MainWindowViewModel.cs

--- TestPanel/ViewModels/MainWindowViewModel.cs
+++ TestPanel/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using Avalonia.Layout;
 using ReactiveUI;
 
 namespace TestPanel.ViewModels;
@@ -6,11 +7,32 @@ namespace TestPanel.ViewModels;
 public class MainWindowViewModel : ViewModelBase
 {
     private ObservableCollection<string>? _items;
+    private Orientation _orientation;
+    private double _spacing;
+    private double _itemSize;
 
     public ObservableCollection<string>? Items
     {
         get => _items;
         set => this.RaiseAndSetIfChanged(ref _items, value);
+    }
+
+    public Orientation Orientation
+    {
+        get => _orientation;
+        set => this.RaiseAndSetIfChanged(ref _orientation, value);
+    }
+    
+    public double Spacing
+    {
+        get => _spacing;
+        set => this.RaiseAndSetIfChanged(ref _spacing, value);
+    }
+
+    public double ItemSize
+    {
+        get => _itemSize;
+        set => this.RaiseAndSetIfChanged(ref _itemSize, value);
     }
 
     public MainWindowViewModel()
@@ -23,5 +45,9 @@ public class MainWindowViewModel : ViewModelBase
         {
             _items.Add($"Item {i}");
         }
+
+        _orientation = Orientation.Vertical;
+        _spacing = 1;
+        _itemSize = 100;
     }
 }
